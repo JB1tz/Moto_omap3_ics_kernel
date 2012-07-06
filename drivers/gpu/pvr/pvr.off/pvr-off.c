af1144d61a18a41611d98562e171ad240cd4003e
Jonathan Bither <jonbither@gmail.com>
2012-07-06 02:30:52 -0400

    pvr-off: Cleanup and provide the correct major for our device

--- drivers/gpu/pvr/pvr.off/pvr-off.c
+++ drivers/gpu/pvr/pvr.off/pvr-off.c
@@ -56,7 +56,6 @@ typedef enum _OMAP_ERROR_
 #define	LDM_DEV	struct platform_device
 
 SYMSEARCH_DECLARE_FUNCTION_STATIC(OMAP_ERROR, OMAPLFBDeinit);
-
 SYMSEARCH_DECLARE_FUNCTION_STATIC(IMG_INT, PVRSRVDriverRemove, LDM_DEV *);
 SYMSEARCH_DECLARE_FUNCTION_STATIC(IMG_VOID, PVRMMapCleanup);
 SYMSEARCH_DECLARE_FUNCTION_STATIC(IMG_VOID, LinuxMMCleanup);
@@ -67,9 +66,8 @@ SYMSEARCH_DECLARE_ADDRESS_STATIC(rfkill_fops);
 static int __exit OMAPLFBDriverRemove_Entry(struct platform_device *pdev)
 {
 	if(OMAPLFBDeinit() != OMAP_OK)
-	{
 		printk(KERN_WARNING "PVR-off: OMAPLFBDriverRemove: OMAPLFBDeinit failed\n");
-	}
+
 	return 0;
 }
 
@@ -96,19 +94,18 @@ static struct class *psPvrClass;
 
 static int find_pvr_class_struct(void)
 {
-    /* rfkill_fops - the last symbol in kallsyms before the strings */
+	/* rfkill_fops - the last symbol in kallsyms before the strings */
 	unsigned char *func = (void *)SYMSEARCH_GET_ADDRESS(rfkill_fops);
 	uint pvrClassNameAdr;
 	uint match0 = 0, match1 = 0, match2 = 0, match3 = 0;
 	int i;
 
-    /* find address of 'pvr' string */
-	for(i = 0; i < 0x00100000; i+=1) 
-	{
-	    // looking for ' pvr '
-		if(func[i] == 0x00 && func[i+1] == 0x70 && func[i+2] == 0x76
-			&& func[i+3] == 0x72 && func[i+4] == 0x00)
-		{ 
+	/* find address of 'pvr' string */
+	for (i = 0; i < 0x00100000; i+=1) {
+		// looking for ' pvr '
+		if (func[i] == 0x00 && func[i+1] == 0x70 &&
+		    func[i+2] == 0x76 && func[i+3] == 0x72 &&
+		    func[i+4] == 0x00) {
 			pvrClassNameAdr = (uint)func + i + 1;
 			printk(KERN_INFO "PVR-off: found pvr class name string at 0x%x\n", pvrClassNameAdr);
 			match0 = pvrClassNameAdr & 0xff;
@@ -119,97 +116,92 @@ static int find_pvr_class_struct(void)
 		}
 	}
 
-    if (!pvrClassNameAdr)
-    {
-        printk(KERN_WARNING "PVR-off: pvr class name string not found!\n");
-        return -1;
-    }
+	if (!pvrClassNameAdr) {
+		printk(KERN_WARNING "PVR-off: pvr class name string not found!\n");
+		return -1;
+	}
 
 	/* the class structure begins with pointer to the name string */
 	func = (void *)SGX_BASE_ADDR; /* 0xcec00000 on omap3410 */
 
-	for(i = 0; i < 0x30000000; i+=1)
-	{
-		if(func[i+3] == match3 && func[i+2] == match2
-			&& func[i+1] == match1 && func[i] == match0)
-		{ 
+	for(i = 0; i < 0x30000000; i+=1) {
+		if (func[i+3] == match3 && func[i+2] == match2 &&
+		    func[i+1] == match1 && func[i] == match0) {
 			psPvrClass = (void *)((uint)func + i);
 			printk(KERN_INFO "PVR-off: found pvr class struct at 0x%p\n", psPvrClass);
 			printk(KERN_INFO "PVR-off: name of the found class is %s\n", psPvrClass->name);
 			break;
 		}
 	}
-	if (!psPvrClass)
-	{
+	if (!psPvrClass) {
 	    printk(KERN_WARNING "PVR-off: pvr class structure not found!\n");
 	    return -1;
 	}
+
 	return 0;
 }
 
 static int __init pvroff_init(void)
 {
-    struct device_driver *drv;
-    struct kobject *kobj;
-    IMG_INT AssignedMajorNumber = 252;
+	struct device_driver *drv;
+	struct kobject *kobj;
+	IMG_INT AssignedMajorNumber = 247;
     
 	printk(KERN_INFO "PVR-off: init\n");
-	
-    SYMSEARCH_BIND_FUNCTION(pvr-off, OMAPLFBDeinit);
-    
-    SYMSEARCH_BIND_FUNCTION(pvr-off, PVRSRVDriverRemove);
-    SYMSEARCH_BIND_FUNCTION(pvr-off, PVRMMapCleanup);
-    SYMSEARCH_BIND_FUNCTION(pvr-off, LinuxMMCleanup);
-    SYMSEARCH_BIND_FUNCTION(pvr-off, LinuxBridgeDeInit);
-    SYMSEARCH_BIND_FUNCTION(pvr-off, RemoveProcEntries);
-    SYMSEARCH_BIND_ADDRESS(pvr-off, rfkill_fops);
+	SYMSEARCH_BIND_FUNCTION(pvr-off, OMAPLFBDeinit);
+	SYMSEARCH_BIND_FUNCTION(pvr-off, PVRSRVDriverRemove);
+	SYMSEARCH_BIND_FUNCTION(pvr-off, PVRMMapCleanup);
+	SYMSEARCH_BIND_FUNCTION(pvr-off, LinuxMMCleanup);
+	SYMSEARCH_BIND_FUNCTION(pvr-off, LinuxBridgeDeInit);
+	SYMSEARCH_BIND_FUNCTION(pvr-off, RemoveProcEntries);
+	SYMSEARCH_BIND_ADDRESS(pvr-off, rfkill_fops);
 
-    lock_kernel();
-    
-//OMAPLFB
-    drv = driver_find("omaplfb", &platform_bus_type);
-    if (!drv) {
-        printk(KERN_WARNING "PVR-off: omaplfb driver not found, bailing out!\n");
-        unlock_kernel();
-        return -1;
-    } else {
-        kobj = &drv->p->kobj;
-        drv->remove = platform_drv_remove_omaplfb;
-        driver_unregister(drv);
-        kobject_del(kobj);
-    }
-	
-//PVR
-    if (find_pvr_class_struct())
-    {
-        printk(KERN_WARNING "PVR-off: class structure search failed, bailing out!\n");
-        unlock_kernel();
-        return -1;
-    }
-   	device_destroy(psPvrClass, MKDEV(AssignedMajorNumber, 0));
-    class_destroy(psPvrClass);
-    unregister_chrdev((IMG_UINT)AssignedMajorNumber, "pvrsrvkm");
-    drv = driver_find("pvrsrvkm", &platform_bus_type);
-    if (!drv) {
-        printk(KERN_WARNING "PVR-off: pvrsrvkm driver not found, bailing out!\n");
-        unlock_kernel();
-        return -1;
-    } else {
-        kobj = &drv->p->kobj;
-        drv->remove = platform_drv_remove_pvrsrvkm;
-        driver_unregister(drv);
-        kobject_del(kobj);
-    }
-    pdev->dev.release = PVRSRVDeviceRelease;
-    platform_device_unregister(pdev);
-   	PVRMMapCleanup();
-    LinuxMMCleanup();
-    LinuxBridgeDeInit();
-    RemoveProcEntries();
-    
+	lock_kernel();
+
+	// OMAPLFB
+	drv = driver_find("omaplfb", &platform_bus_type);
+	if (!drv) {
+		printk(KERN_WARNING "PVR-off: omaplfb driver not found, bailing out!\n");
+		unlock_kernel();
+		return -1;
+	} else {
+		kobj = &drv->p->kobj;
+		drv->remove = platform_drv_remove_omaplfb;
+		driver_unregister(drv);
+		kobject_del(kobj);
+	}
+
+	// PVR
+	if (find_pvr_class_struct()) {
+		printk(KERN_WARNING "PVR-off: class structure search failed, bailing out!\n");
+		unlock_kernel();
+		return -1;
+	}
+
+	device_destroy(psPvrClass, MKDEV(AssignedMajorNumber, 0));
+	class_destroy(psPvrClass);
+	unregister_chrdev((IMG_UINT)AssignedMajorNumber, "pvrsrvkm");
+	drv = driver_find("pvrsrvkm", &platform_bus_type);
+	if (!drv) {
+		printk(KERN_WARNING "PVR-off: pvrsrvkm driver not found, bailing out!\n");
+		unlock_kernel();
+		return -1;
+	} else {
+		kobj = &drv->p->kobj;
+		drv->remove = platform_drv_remove_pvrsrvkm;
+		driver_unregister(drv);
+		kobject_del(kobj);
+	}
+
+	pdev->dev.release = PVRSRVDeviceRelease;
+	platform_device_unregister(pdev);
+	PVRMMapCleanup();
+	LinuxMMCleanup();
+	LinuxBridgeDeInit();
+	RemoveProcEntries();
    	unlock_kernel();
 
-    printk(KERN_INFO "PVR-off: successfully done\n");
+	printk(KERN_INFO "PVR-off: successfully done\n");
 	return 0;
 }
 
